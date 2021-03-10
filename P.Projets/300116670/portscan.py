42f1160ad51b025eb902c414f8187b10ac0b9d1e
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2021-03-10 18:33:29 -0500

    Update portscan.py

--- P.Projets/300116670/portscan.py
+++ P.Projets/300116670/portscan.py
@@ -1,1 +1,50 @@
+import socket 
+import subprocess
+import sys
+from datetime import datetime
+
+#Blank your screen
+subprocess.call('clear', shell=True)
+
+#Ask for input 
+remoteServer = raw input("Enter a remote host to scan: ")
+remoteServerIP = socket.gethostbyname(remoteServer)
+
+#Print a nice banner with information on which host to scan
+print "_" * 60
+print "Please wait, scanning remotye host", remoteServerIP
+print "_" * 60
+
+#Check the date and time the scan was started 
+t1 = datetime.now()
+
+#Using the range function specify ports.
+#Also we will do error handling
+
+try:
+    for port in range (1.5000):
+        sock = socket.socket(socket.AF_INET, socket.SOC_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0
+            print "Port {}:     Open".format(port)
+        sock.close()
+except KeyboardInterrupt:
+    print "You pressed Ctrl+C"
+    sys.exit()
+
+except socket.gaierror:
+    print "Hostname could not be resolved. Exiting"
+    sys.exit()
+
+except socket.error:
+    print "Couldn't connect to server"
+    sys.exit()
+#Checking time again
+t2 = datetime.now()
+
+#Calculate the difference in time to know how long the scan took
+total = t2 - t1
+
+#Printing the information on the screen
+print 'Scanning Completed in: ', total
 
