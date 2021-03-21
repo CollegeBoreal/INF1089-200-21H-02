7f852d9ad9b498ba014ecae6dbb67d04e2d1719e
fadde68 <54910778+fadde68@users.noreply.github.com>
2021-03-21 18:56:45 -0400

    😎😎

---
+++ 5.Powershell/300115065/b300115065-structure.ps1
@@ -0,0 +1,30 @@
+<#
+.SYNOPSIS
+    Ce script est une laboratoire Powershell
+.DESCRIPTION
+    Ce script est utilisé pour le laboratoire de programmation en Powershell.
+.NOTES
+    Author: 300115065
+    Derniere mise à jour: yyyy-mm-dd
+#>
+
+[String]$personneNom = "Alice"
+[Int]$personneAge = 35
+
+Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
+
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
+
+$villes = "Toronto","Mississauga","Scarborough","Brampton"
+
+for ($i = 0; $i -le $villes.Count; $i = $i + 1) {
+    $villes[$i]
+}
+
+Clear-Host
