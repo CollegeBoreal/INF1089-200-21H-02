21dbb62c78e386109688c24aa145f817104fb64f
zoureni <zoureni@outlook.fr>
2021-03-21 12:29:02 -0400

    zoureni

---
+++ 5.Powershell/300118524/b000000000-structure.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+    Ce script est une laboratoire Powershell
+
+.DESCRIPTION
+    Ce script est utilisé pour le laboratoire de programmation en Powershell.
+
+.NOTES
+    Author: CollegeBorealzoureni
+    Derniere mise à jour: 2020-04-14
+
+#>
+
+[String]$personneNom = "Alice"
+[Int]$personneAge = 35
+Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
+$villes = "Toronto","Mississauga","Scarborough","Brampton"
+for ($i = 0; $i -le $villes.Count; $i = $i + 1) {
+    $villes[$i]
+}
+clear-Host
+"{0}" -F ($villes.Count % 2)
