a5297ae429663d96856e880863790a2be0efc683
Boris Kwayep <Boriskwayep35@yahoo.com>
2021-03-28 14:59:08 -0400

    Update LogAppView.ps1

--- P.Projets/300117178/LogAppView.ps1
+++ P.Projets/300117178/LogAppView.ps1
@@ -1,3 +1,5 @@
+param([Int32]$days=1)
+
 $css= "<style>"
 $css= $css+ "BODY{ text-align: center; background-color:white;}"
 $css= $css+ "TABLE{    font-family: 'Lucida Sans Unicode', 'Lucida Grande', Sans-Serif;font-size: 12px;margin: 10px;width: 100%;text-align: center;border-collapse: collapse;border-top: 7px solid #004466;border-bottom: 7px solid #004466;}"
@@ -6,7 +8,7 @@ $css= $css+ "TD{padding: 1px;background: #e5f7ff;border-right: 1px solid #004466
 $css= $css+  "TD:hover{ background-color:#004466;}"
 $css= $css+ "</style>" 
  
-$StartDate = (get-date).adddays(-1)
+$StartDate = (get-date).adddays(-$days)
  
 $body = Get-WinEvent -FilterHashtable @{logname="Application"; starttime=$StartDate} -ErrorAction SilentlyContinue
  
