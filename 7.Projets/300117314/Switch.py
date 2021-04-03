dfacb3241931a6c08558d61531cf9213abf3e3c6
morti747 <54910700+morti747@users.noreply.github.com>
2021-04-03 17:26:32 -0400

    Update Switch.py

--- 7.Projets/300117314/Switch.py
+++ 7.Projets/300117314/Switch.py
@@ -26,15 +26,42 @@ time.sleep(2)
 
 
 
-
-
 for n in range(2,10):
     tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
     time.sleep(2)
     tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
     
+time.sleep(2)
+tn.write(b'interface fastEthernet 0/1\n')
+time.sleep(2)
+tn.write(b'switchport mode access\n')
+time.sleep(2)
+tn.write(b'switchport access vlan 2\n')
+time.sleep(2)
 
+time.sleep(2)
+tn.write(b'interface fastEthernet 0/2\n')
+time.sleep(2)
+tn.write(b'switchport mode access\n')
+time.sleep(2)
+tn.write(b'switchport access vlan 3\n')
+time.sleep(2)
 
+time.sleep(2)
+tn.write(b'interface fastEthernet 0/3\n')
+time.sleep(2)
+tn.write(b'switchport mode access\n')
+time.sleep(2)
+tn.write(b'switchport access vlan 4\n')
+time.sleep(2)
+
+time.sleep(2)
+tn.write(b'interface fastEthernet 0/4\n')
+time.sleep(2)
+tn.write(b'switchport mode access\n')
+time.sleep(2)
+tn.write(b'switchport access vlan 5\n')
+time.sleep(2)
 
 
 tn.write(b'end\n')
