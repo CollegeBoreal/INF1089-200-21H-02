9be3fccc1ccde23cb891eaca4265597eed0a7b1f
b300098957 <b300098957@collegeboreal.ca>
2021-04-07 13:05:37 -0400

    .

---
+++ 7.Projets/.scripts/participation.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# --------------------------------------
+#
+#
+#
+# --------------------------------------
+
+source ../.scripts/students.sh --source-only
+
+   
+echo "# Participation au `date +"%d-%m-%Y %H:%M"`"
+echo ""
+
+
+echo "| Table des matières            | Description                                             |"
+echo "|-------------------------------|---------------------------------------------------------|"
+echo "| :a: [Présence](#a-présence)   | L'étudiant.e a fait son travail    :heavy_check_mark:   |"
+echo "| :b: [Précision](#b-précision) | L'étudiant.e a réussi son travail  :tada:               |"
+
+echo ""
+echo "## Légende"
+echo ""
+
+echo "| Signe              | Signification                 |"
+echo "|--------------------|-------------------------------|"
+echo "| :heavy_check_mark: | Prêt à être corrigé           |"
+echo "| :x:                | Projet inexistant             |"
+
+
+echo ""
+echo "## :a: Présence"
+echo ""
+echo "|:hash:| Boréal :id:                | Yaml               |"
+echo "|------|----------------------------|--------------------|"
+
+OK=":heavy_check_mark:"
+KO=":x:"
+
+i=1
+
+for id in "${ETUDIANTS[@]}"
+do
+   VALUE="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> |"
+  
+   DIR=${id}
+   if [ -d "$DIR" ]; then
+       VALUE="${VALUE} [${OK} ${id}](../${DIR}) |"
+   else
+       VALUE="${VALUE} ${KO} |"
+   fi
+
+   FILE=${id}/README.md
+   if [ -f "$FILE" ]; then
+       VALUE="${VALUE} [${OK} ${id}](../${FILE}) |"
+   else
+       VALUE="${VALUE} ${KO} |"
+   fi
+   echo ${VALUE}
+   let "i++"
+
+done
