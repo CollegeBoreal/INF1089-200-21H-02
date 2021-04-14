af13568fb078e1de8cccc544e2d4137209cbcdb4
Auriane25 <55238107+Auriane25@users.noreply.github.com>
2021-04-14 13:23:19 -0400

    Update monscript.sh

--- 6.CI-CD/300116670/monscript.sh
+++ 6.CI-CD/300116670/monscript.sh
@@ -5,6 +5,6 @@ printf "Current date and time %s\n" "$now"
 now="$(date +'%d/%m/%Y')"
 printf "Current date in dd/mm/yyyy format %s\n" "$now"
  
-echo "Starting backup at $now, please wait..."
+echo "Starting backup at $now, please wait..." > 30011667/date.txt
 # command to backup scripts goes here
 # ...
