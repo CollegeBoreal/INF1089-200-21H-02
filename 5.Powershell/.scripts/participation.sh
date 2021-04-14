2d4cbb1732c4d9839f21d6adb82d7338a1301b2b
b300098957 <b300098957@collegeboreal.ca>
2021-04-14 12:22:35 -0400

    .

--- 5.Powershell/.scripts/participation.sh
+++ 5.Powershell/.scripts/participation.sh
@@ -31,20 +31,30 @@ echo "| :x:                | Projet inexistant             |"
 echo ""
 echo "## :a: Présence"
 echo ""
-echo "|:hash:| Boréal :id:                | Fait               |"
-echo "|------|----------------------------|--------------------|"
+echo "|:hash:| Boréal :id:                | Fonction           | Structure |"
+echo "|------|----------------------------|--------------------|-----------|"
 
-i=1
+i=0
+OK=":white_check_mark:"
+KO=":x:"
 
 for id in "${ETUDIANTS[@]}"
 do
-   FILE=${id}/b${id}-fonction.ps1
-   OK="| ${i} | [${id}](../${FILE}) | [:heavy_check_mark:] | "
-   KO="| ${i} | [${id}](../${FILE}) | [:x:]                | "
-   if [ -f "$FILE" ]; then
-       echo ${OK}
+   VALUE="| ${i} | ${id} - <image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> |"
+
+   FONCTION=${id}/b${id}-fonction.ps1
+   if [ -f "$FONCTION" ]; then
+       VALUE="${VALUE} [:while_check_mark: ${FONCTION}](../${FONCTION}) |"
    else
-       echo ${KO}
+       VALUE="${VALUE} ${KO} |"
    fi
+
+   STRUCTURE=${id}/b${id}-structure.ps1
+   if [ -f "$STRUCTURE" ]; then
+       VALUE="${VALUE} [:while_check_mark: ${STRUCTURE}](../${STRUCTURE}) |"
+   else
+       VALUE="${VALUE} ${KO} |"
+   fi
+   echo ${VALUE}
    let "i++"
 done
