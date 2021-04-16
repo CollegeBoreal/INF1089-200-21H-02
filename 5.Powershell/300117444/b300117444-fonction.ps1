f1ae8e482c1eac5a24f202d924816ef2d9cf5a0f
Idir <54910261+dido05327@users.noreply.github.com>
2021-04-16 16:00:09 -0400

    Update b300117444-fonction.ps1

--- 5.Powershell/300117444/b300117444-fonction.ps1
+++ 5.Powershell/300117444/b300117444-fonction.ps1
@@ -1,30 +1,43 @@
 <#
 .SYNOPSIS
     Ce script est un laboratoire Powershell
-
 .DESCRIPTION
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
-
 .NOTES
     Author: CollegeBoreal
     Derniere mise à jour: yyyy-mm-dd
-
 #>
-
 # Definition de la fonction
 function Stagiaire {
+
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [parameter(Mandatory=$true)]
         [String]$personneNom,
-        [Parameter(Mandatory=$true)]
-        [Int]$personneAge
+
+        
+        [parameter(Mandatory=$true)]
+        [ValidateRange(10,99)]
+
+        [Int32]$personneAge
     )
     # message de bienvenue 
     BEGIN {Write-Verbose "Début du script"}
     PROCESS { "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge }
-    END {Write-Verbose "Fin du script"}}
+    END {Write-Verbose "Fin du script"}
+
+}
+
+
+
+
 
 # Appel de la fonction
-Stagiaire Toronto 35
+
+Stagiaire
+
+Stagiaire "Pascal Siakam"
+
+Stagiaire "Pascal Siakam" 1238  
+
 Stagiaire "Pascal Siakam" 26  -verbose
