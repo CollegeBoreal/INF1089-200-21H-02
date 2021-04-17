eba3dce0a5291df44d7a5c89b2d31b2217e7c3c5
Sekou16 <55207099+Sekou16@users.noreply.github.com>
2021-04-17 12:27:23 -0400

    Update b300111441-fonction.ps1

--- 5.Powershell/300111441/b300111441-fonction.ps1
+++ 5.Powershell/300111441/b300111441-fonction.ps1
@@ -6,30 +6,47 @@
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
 
 .NOTES
-    Author: CollegeBoreal
+    Author: Sekou
     Derniere mise à jour: yyyy-mm-dd
 
+.PARAMETER personneNom
+c'est le nom de l'utilisateur
+
+.PARAMETER personneAge
+c'est l'âge de l'utilisateur
+
+.PARAMETER personneEntreprise
+c'est l'entrprise l'utilisateur fréquente
+
+.PARAMETER personneProfession
+c'est la profession de l'utilisateur
 #>
 
 # Definition de la fonction
-function Stagiaire {
+function information_travailleur {
 
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]
+        [Alias("Nom_du_travailleur")]
         [String]$personneNom,
         
         [Parameter(Mandatory=$true)]
+        [Alias("age_du_travailleur")]
         [ValidateRange(7,77)]
         [Int32]$personneAge
-    )
-    # message de bienvenue 
-    BEGIN {Write-Verbose "Début du script."}
-    PROCESS { "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge }
-    END {Write-Verbose "Fin du script."}
 
+        [Parameter(Mandatory=$true)]
+        [Alias("profession_du_travailleur)]
+        [String]$personneProgramme     
+    )
+
+    # message de bienvenue 
+    BEGIN {Write-Verbose "Début du script"}
+    PROCESS { "Bonjour {0} ! Tu as {1} ans. tu est inscrit au {2} au programme de {3}" -F $personneNom, $personneAge, $personneCollege, $personneProgramme }       
+    END {Write-Verbose "Fin du script"}
 }
 
 # Appel de la fonction
-Stagiaire "Toronto" 35
-  
+Get-Help information_du_travailleur
+information_etudiant -verbose
