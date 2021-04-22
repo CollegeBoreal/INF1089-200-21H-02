794505f4ea7d7bd90e9e0194b37a5ff745192e70
morti747 <54910700+morti747@users.noreply.github.com>
2021-04-22 16:39:58 -0400

    Create No_vlan.py

---
+++ 7.Projets/300117314/No_vlan.py
@@ -0,0 +1,43 @@
+import getpass
+import sys
+import telnetlib
+import time
+
+Host="10.13.237.211"
+
+user=input(' Enter User name: ')
+password=getpass.getpass()
+
+tn = telnetlib.Telnet(Host)
+tn.read_until(b'Username: ')
+tn.write(user.encode('ascii') + b'\n')
+
+if password:
+    tn.read_until(b'Password: ')
+    tn.write(password.encode('ascii')+b'\n')
+
+time.sleep(2)
+tn.write(b'enable\n')
+time.sleep(2)
+tn.write(b'cisco\n')
+time.sleep(2)
+tn.write(b'config t\n')
+time.sleep(2)
+
+
+
+
+
+for n in range(2,10):
+    tn.write(b"no vlan " + str(n).encode('ascii') + b"\n")
+    time.sleep(2)
+    
+    
+
+
+
+
+tn.write(b'end\n')
+tn.write(b'exit\n')
+line=tn.read_all()
+print (line)
